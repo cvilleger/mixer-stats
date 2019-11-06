d46ef2773135facc7df453ef7d5068fcfc7aeae2
cvilleger <villeger.c@gmail.com>
2019-11-06 21:45:29 +0100

    init

--- src/Service/MixerService.php
+++ src/Service/MixerService.php
@@ -4,19 +4,15 @@ namespace App\Service;
 
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-class MixerService
-{
+class MixerService{
     private $httpClient;
 
-    public function __construct(HttpClientInterface $httpClient)
-    {
+    public function __construct(HttpClientInterface $httpClient){
         $this->httpClient = $httpClient;
     }
 
-    public function getData(): array
-    {
+    public function getData(): array{
         $url = 'https://mixer.com/api/v1/channels?order=viewersCurrent:DESC&limit=20&where=languageId:eq:fr';
         $response = $this->httpClient->request('GET', $url);
-        return $response->toArray();
-    }
+        return $response->toArray();}
 }
